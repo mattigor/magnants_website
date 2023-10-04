f63cecbdc1740366f1dec2ea07ab69b5beb457c9
Igor Matheus <mattigor.impr@gmail.com>
2023-10-03 21:44:20 -0300

    upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Esta função verifica qual seção está ativa na barra de navegação do site
+function atualizarLinkAtivo() {
+  const links = document.querySelectorAll('nav ul li a');
+
+  links.forEach((link) => {
+      const sectionId = link.getAttribute('href').substring(1);
+      const section = document.getElementById(sectionId);
+      if (
+          section.offsetTop <= window.scrollY &&
+          section.offsetTop + section.offsetHeight > window.scrollY
+      ) {
+          link.classList.add('ativo'); // adiciona a classe .ativo, onde está o background laranja
+      } else {
+          link.classList.remove('ativo');
+      }
+  });
+}
+window.addEventListener('scroll', atualizarLinkAtivo);
+window.addEventListener('load', atualizarLinkAtivo);
