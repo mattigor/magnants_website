8049a34c24444f5daf183305239a0ba590f5ce61
Igor Matheus <mattigor.impr@gmail.com>
2024-03-28 08:44:12 -0300

    fix

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // Esta função verifica qual seção está ativa na barra de navegação do site
 function atualizarLinkAtivo() {
-  const links = document.querySelectorAll('nav ul li a');
-
-  links.forEach((link) => {
-      const sectionId = link.getAttribute('href').substring(1);
-      const section = document.getElementById(sectionId);
-      if (
-          section.offsetTop <= window.scrollY &&
-          section.offsetTop + section.offsetHeight > window.scrollY
-      ) {
-          link.classList.add('ativo'); // adiciona a classe .ativo, onde está o background laranja
-      } else {
-          link.classList.remove('ativo');
-      }
-  });
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach((link) => {
+        const sectionId = link.getAttribute("href").substring(1);
+        const section = document.getElementById(sectionId);
+        const sectionTop = section.offsetTop - window.innerHeight / 2;
+        const sectionBottom = sectionTop + section.offsetHeight;
+        
+        if (sectionTop <= window.scrollY && sectionBottom > window.scrollY) {
+            link.classList.add("ativo");
+        } else {
+            link.classList.remove("ativo");
+        }
+    });
 }
-window.addEventListener('scroll', atualizarLinkAtivo);
-window.addEventListener('load', atualizarLinkAtivo);
+window.addEventListener("scroll", atualizarLinkAtivo);
+window.addEventListener("load", atualizarLinkAtivo);
 
 // Função que adiciona animação ao menu hamburguer
 function rotateBars(bar) {
